baed4b5d803e38193fc0564a107be5c5676211e9
felipemiramontesr <felipemiramontesr@gmail.com>
2026-04-29 16:25:16 -0600

    🔱 ARCHON HOUSE CLEANING - Purged obsolete migration 060; established 070 as the sovereign database baseline

--- packages/database/recent/u701509674_Mant_piic.sql
+++ packages/database/recent/u701509674_Mant_piic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 29-04-2026 a las 03:46:00
+-- Tiempo de generación: 29-04-2026 a las 20:16:52
 -- Versión del servidor: 11.8.6-MariaDB-log
 -- Versión de PHP: 7.2.34
 
