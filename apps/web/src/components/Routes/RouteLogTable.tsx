4f006b5e3bfdde264aa1a517240886c4d2aadae2
felipemiramontesr <felipemiramontesr@gmail.com>
2026-05-08 18:52:40 -0600

    V.78.32.0_Industrial_Structural_Purge

--- apps/web/src/components/Routes/RouteLogTable.tsx
+++ apps/web/src/components/Routes/RouteLogTable.tsx
@@ -97,7 +97,7 @@ const RouteLogRow = ({
         transition={{ delay: index * 0.05 }}
         onClick={onToggle}
         className={`cursor-pointer transition-all duration-300 hover:bg-[#0f2a44]/[0.02] ${
-          isExpanded ? 'expanded-focus-blue shadow-inner' : ''
+          isExpanded ? 'expanded-focus-blue' : ''
         }`}
       >
         {/* Operador */}
